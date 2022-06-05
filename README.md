fd0cf0aea9eff19e153781eb5a99b7f9bb660e5a
asalek <asalek8599@gmail.com>
2022-06-05 18:07:58 +0100

    push: weather project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Weather Status
+
+## how to use
+
+```bash
+git clone
+cd weatherProject
+```
+
+
+### in case
+npm not working :
+```bash
+npm init
+npm i express
+npm i nodemon
+npx nodemon script.js
+```
---
+++ script.js
@@ -0,0 +1,26 @@
+
+const { response } = require("express");
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+	
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=Casablanca&appid=2346317352a24f926b0a0ad2dfa70b6e&unit=metric";
+	
+	https.get(url, function(response){
+		//console.log(response);
+		response.on("data", function(data){
+			const wData = JSON.parse(data); // # JSON.stringify(data) => from json to string
+			console.log(wData.weather[0].main);
+		});
+	})
+	res.sendFile(__dirname + "/index.html");
+});
+
+
+
+app.listen(3000, function(){
+	console.log("server running on port 3000");
+})
