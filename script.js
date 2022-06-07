7043ac8cd85a77c6aeab4b10dd25dbc50a65d552
Phoenix <phoenix@localhost.localdomain>
2022-06-07 17:18:56 +0100

    update: celsuis and weather status icon

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ app.get("/", function(req, res){
 	const url = "https://api.openweathermap.org/data/2.5/weather?q=Casablanca&appid=2346317352a24f926b0a0ad2dfa70b6e&units=metric";
 	
 	https.get(url, function(response){
-		//console.log(response);
 		response.on("data", function(data){
 			const wData = JSON.parse(data); // # JSON.stringify(data) => from json to string
 			const wstatus = wData.weather[0].description;
@@ -24,7 +23,6 @@ app.get("/", function(req, res){
 			res.send();
 		});
 	})
-	// res.sendFile(__dirname + "/index.html");
 });
 
 
